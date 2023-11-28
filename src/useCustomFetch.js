514db72e9124f4196c32a15a8c3c2ef3a6bfda26
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2023-11-28 19:59:49 +0600

    fetched data using async await

--- src/useCustomFetch.js
+++ src/useCustomFetch.js
@@ -6,14 +6,27 @@ export const useFetch = (url) => {
   const [err, setErr] = useState(null);
 
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setValues(data))
-      .catch((err) => setErr(err.message));
+    //   fetch(url)
+    //     .then((res) => res.json())
+    //     .then((data) => setValues(data))
+    //     .catch((err) => setErr(err.message));
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Failed to fetch data!");
+        }
+        const data = await response.json();
+        setValues(data);
+      } catch (error) {
+        setErr(error.message);
+      }
+    };
+    fetchData();
   }, [url]);
 
-  return { 
+  return {
     values,
-    err
+    err,
   };
 };
