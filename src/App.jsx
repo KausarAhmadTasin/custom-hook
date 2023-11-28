c8d5fd5317b7e3a0d50be333f6159798d4be5155
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2023-11-28 17:20:17 +0600

    added comment and album cards

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ function App() {
 
   const { values, err } = useFetch(postUrl);
   const { values: commentValues, err: commentErr } = useFetch(commentsUrl);
-  const { values: albaumValues , err: albumErr} = useFetch(albumUrl);
+  const { values: albaumValues, err: albumErr } = useFetch(albumUrl);
   // console.log(values)
   // console.log(albaumValues)
+  
   return (
     <>
       <h2>Users</h2>
@@ -19,53 +20,43 @@ function App() {
         {values.map((value) => {
           return (
             <div key={value.id} className="post-api">
-              <p>
-                <b>Id:</b> {value.id}
-              </p>
-              <p>
-                <b>User Id:</b> {value.userId}
-              </p>
-              <p>
-                <b>Title:</b> {value.title}
-              </p>
-              <p>
-                <b>Post:</b> {value.body}
-              </p>
+              {err && { err }}
+              <p><b>Id:</b> {value.id}</p>
+              <p><b>User Id:</b> {value.userId}</p>
+              <p><b>Title:</b> {value.title}</p>
+              <p><b>Post:</b> {value.body}</p>
               {/* {console.log(value)} */}
             </div>
           );
         })}
-        {err && {commentErr}}
       </div>
       <br />
       <h2>Comments</h2>
       <br />
       <div className="comment-container">
-        {
-          commentValues.map((value) => (
-            <div key={value.id} className="comment-api">
-              <p> <b>Name: </b> {value.name}</p>
-              <p><b>Email: </b>{value.email}</p>
-              <p><b>Comment body: </b>{value.body}</p>
-            </div>
-          ))
-        }
+        {commentValues.map((value) => (
+          <div key={value.id} className="comment-api">
+            {commentErr && {commentsUrl}}
+            <p><b>Name: </b> {value.name}</p>
+            <p><b>Email: </b>{value.email}</p>
+            <p><b>Comment body: </b>{value.body}</p>
+          </div>
+        ))}
       </div>
 
       <br />
       <h2>Album</h2>
       <br />
       <div className="album-container">
-      {
-        albaumValues.map((value) => (
+        {albaumValues.map((value) => (
           // {console.log(value.id)}
           <div className="album-api" key={value.id}>
+            {albumErr && {albumErr}}
             <p><b>User Id: </b>{value.id}</p>
             <p><b>Album Id: </b>{value.userId}</p>
             <p><b>Album Title </b>{value.title}</p>
           </div>
-        ))
-      }
+        ))}
       </div>
     </>
   );
